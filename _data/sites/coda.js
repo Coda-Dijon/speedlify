f59c13b8aa6fde26752efaf8c8a59ed9cce4c51d
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-29 11:35:56 +0200

    feat: add more pages

--- _data/sites/coda.js
+++ _data/sites/coda.js
@@ -1,18 +1,18 @@
 module.exports = {
 	name: "Coda",
-	description: "The default sites that get tested",
+	description: "Coda website",
 	options: {
 		runs: 1,
 		frequency: 1
 	},
 	urls: [
 		"https://www.coda.school/",
-        /*"https://www.coda.school/ecole-informatique",
+        "https://www.coda.school/ecole-informatique",
         "https://www.coda.school/formation-informatique",
         "https://www.coda.school/formation/bachelor-informatique",
         "https://www.coda.school/alternance",
         "https://www.coda.school/campus/dijon",
         "https://www.coda.school/blog",
-        "https://www.coda.school/journees-portes-ouvertes#events"*/
+        "https://www.coda.school/journees-portes-ouvertes#events"
 	]
 };
