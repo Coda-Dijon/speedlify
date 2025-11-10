6c3efba14526765203f839b10f181909d61ef23d
Yoan Thirion <yoan.thirion@gmail.com>
2025-11-10 07:08:43 +0100

    fix: update urls

--- _data/sites/coda.js
+++ _data/sites/coda.js
@@ -1,11 +1,17 @@
 module.exports = {
-	name: "Others",
-	description: "Autres sites",
-	options: {
-		runs: 1,
-		frequency: 1
-	},
-	urls: [
-		"https://coda-school.github.io/advent-2025/"
-	]
+    name: "Coda",
+    description: "Coda website",
+    options: {
+        runs: 1,
+        frequency: 1
+    },
+    urls: [
+        "https://www.coda.school/",
+        "https://www.coda.school/ecole-informatique",
+        "https://www.coda.school/formation-informatique",
+        "https://www.coda.school/formation/bachelor-informatique",
+        "https://www.coda.school/alternance",
+        "https://www.coda.school/campus/dijon",
+        "https://www.coda.school/blog",
+    ]
 };
